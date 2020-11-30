647b14bc24cef0bf1df98ceab3837279aba67993
Miyuko <v.wastiau1994@gmail.com>
2020-11-30 15:19:18 +0100

    Exercice 18 : Titre et flèches

--- calendar.php
+++ calendar.php
@@ -2,6 +2,10 @@
 
 include "calendarFunctions.php";
 
+$annee = $_GET["annee"];
+$annee_suiv = $annee + 1;
+$annee_prev = $annee - 1;
+
 $html = <<<HTML
  <!doctype html>
     <html lang="fr">
@@ -12,19 +16,50 @@ $html = <<<HTML
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.css" />
         </head>
- 
         <body>
+            <h1>Calendrier $annee</h1>
+
             <div class = 'container'>
-                <div class = 'row'>
+                
 HTML;
 
-$annee = $_GET["annee"];
+for ($i = 0; $i <= 2; $i++){
+    $html .="<div class = 'row'>";
+    if ($i == 0){
 
-                for ($i= 1; $i <= 12; $i++){
-                    $html .= "<div class = 'col-md-4'>";
-                    $html .= (calendar($i, $annee, True));
-                    $html .= "</div>";
-                }     
+        $html .=<<<HTML
+        <div class = 'col-md-2 precedent'>
+            <a href = 'calendar.php?annee=$annee_prev'><-</a>
+        </div>
+HTML;
+
+    } else {
+        $html .= "<div class = 'col-md-2'></div>";
+    }
+
+    for ($j= 1; $j <= 4; $j++){
+
+        $html .= "<div class='col-md-2'>";
+
+        $html .= calendar($i*4+$j, $annee, False);
+        $html .= "</div>";
+    }
+    if ($i == 0){
+
+        $html .=<<<HTML
+        <div class = 'col-md-2 suivant'>
+            <a href = 'calendar.php?annee=$annee_suiv'>-></a>
+        </div>
+HTML;
+
+    } else {
+        $html .= "<div class = 'col-md-2'></div>";
+    }
+    $html .= "</div>";
+}
+
+
+
 
 $html .=<<<HTML
                 </div>
@@ -34,4 +69,3 @@ $html .=<<<HTML
 HTML;
 
 echo($html);
-
